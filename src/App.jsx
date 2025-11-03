60f72cfd0ca1e8eecde67e28d20b006de4ee2eae
coala <eduagame330@gmail.com>
2025-11-03 16:39:49 -0300

    feat: update user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Index";
 import List from "./pages/List";
+import UpdateUser from "./pages/UpdateUser";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<List />} />
+          <Route path="/update" element={<UpdateUser />} />
         </Routes>
       </main>
     </>
